fc42cc765c726805e1f4f774a5318637fa406067
tanmay711 <tanmaybodwad321@gmail.com>
2026-09-02 21:38:25 +0530

    Time: 2 ms (55.56%), Space: 8.5 MB (30.46%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -3,21 +3,23 @@ public:
     int ans=0;
     bool issafe(int row, int col, vector<vector<char>> &board, int n)
     {
-        for(int j=0;j<col;j++)
+        for(int i=0;i<col;i++)
+
         {
-            if(board[row][j]=='Q')
+            if(board[row][i]=='Q')
             {
                 return false;
             }
+
         }
-        for(int i=row, j=col;i>=0 && j>=0;i--, j--)
+        for(int i=row, j=col;i>=0 && j>=0;i--,j--)
         {
             if(board[i][j]=='Q')
             {
                 return false;
             }
         }
-        for(int i=row, j=col;i<n && j>=0 ; i++, j--)
+        for(int i=row, j=col ; i<n && j>=0;i++, j--)
         {
             if(board[i][j]=='Q')
             {
@@ -33,11 +35,11 @@ public:
             ans++;
             return;
         }
-        for(int row = 0; row<n;row++)
+        for(int row=0;row<n;row++)
         {
             if(issafe(row, col, board, n))
             {
-                board[row][col]='Q';
+                board[row][col] = 'Q';
                 solve(col+1, board, n);
                 board[row][col] = '.';
             }
@@ -45,7 +47,7 @@ public:
     }
     int totalNQueens(int n) {
         vector<vector<char>> board(n, vector<char>(n,'.'));
-        solve(0,board, n);
+        solve(0, board, n);
         return ans;
     }
 };
