52e20e0ecdf012f326c559a278d9d2fbfb390fb5
tanmay711 <tanmaybodwad321@gmail.com>
2026-09-05 00:06:30 +0530

    Time: 0 ms (100%), Space: 31.3 MB (46.94%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -4,11 +4,13 @@ public:
         int n=nums.size();
         vector<int> minn(n);
         minn[n-1] = nums[n-1];
-        for(int i=n-2; i>=0;i--)
+
+        for(int i=n-2;i>=0;i--)
         {
-            minn[i] = min(nums[i],minn[i+1]);
+            minn[i] = min(minn[i+1],nums[i]);
         }
-        int maxy=0;
+
+        int maxy = 0;
         for(int i=0;i<n;i++)
         {
             maxy = max(maxy, nums[i]);
@@ -16,10 +18,13 @@ public:
             int instability = maxy - minn[i];
 
             if(instability <= k)
+
             {
                 return i;
             }
+
         }
-        return -1;
+            return -1;
+
     }
 };
