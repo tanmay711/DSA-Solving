264e22925d8fe026087572464200caf23e3efb94
tanmay711 <tanmaybodwad321@gmail.com>
2026-09-23 12:27:52 +0530

    Time: 2 ms (74.48%), Space: 102.3 MB (65.24%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,44 @@
+// find largest subarray whose sum is total sum - x;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n=nums.size();
+        int totalsum=0;
+        for(int num:nums)
+        {
+            totalsum += num;
+        }
+        int target = totalsum-x;
+        if(target<0)
+        {
+            return -1;
+        }
+        if(target==0)
+        {
+            return n;
+        }
+        int left=0;
+        int maxlen = -1;
+        int sum=0;
+        for(int right= 0; right<n;right++)
+        {
+            sum += nums[right];
+            while(sum > target)
+            {
+                sum -= nums[left];
+                left++;
+            }
+            if(sum == target)
+            {
+                maxlen = max(maxlen, right - left+1);
+            }
+
+        }
+            if(maxlen == -1)
+            {
+                return -1;
+            }
+            return n - maxlen;
+    }
+};
