a1f3810b0f6e7cb9441e51eec52561abd092de26
tanmay711 <tanmaybodwad321@gmail.com>
2026-09-25 20:10:30 +0530

    Time: 30 ms (88.14%), Space: 53.4 MB (82.93%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool palindrome(string s, int start, int end)
+    bool palindrome(string &s,int start,int end)
     {
         while(end>start)
         {
-            if(s[start] != s[end])
+            if(s[start]!= s[end])
             {
                 return false;
             }
@@ -13,28 +13,27 @@ public:
         }
         return true;
     }
-    void solve(string &s, int start, vector<string> &curr, vector<vector<string>> &ans)
+    void solve(string &s, int start, vector<vector<string>> &ans, vector<string> &curr)
     {
-        if(start==s.size())
+        if(start == s.size())
         {
             ans.push_back(curr);
             return;
         }
-        for(int end = start;end<s.size();end++)
+        for(int end=start;end<s.size();end++)
         {
             if(palindrome(s,start,end))
             {
                 curr.push_back(s.substr(start,end-start+1));
-                solve(s, end+1, curr, ans);
+                solve(s, end+1,ans,curr);
                 curr.pop_back();
-                
             }
         }
     }
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
+        vector<vector<string>>ans;
         vector<string> curr;
-        solve(s,0,curr,ans);
+        solve(s, 0, ans, curr);
         return ans;
     }
 };
